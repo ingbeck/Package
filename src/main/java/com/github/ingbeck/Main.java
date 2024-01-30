933d1ff642a7ca2409c884679642ab6620bb6e23
ingobecker <mail@ingobecker.de>
2024-01-30 15:14:21 +0100

    add class and instances of Book.java

--- src/main/java/com/github/ingbeck/Main.java
+++ src/main/java/com/github/ingbeck/Main.java
@@ -2,6 +2,14 @@ package com.github.ingbeck;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Book[] books = new Book[7];
+        books[0] = new Book("HarryPotter und der Stein der Weisen", "J.K. Rowling", "978-0-5450-6967-0");
+        books[1] = new Book("HarryPotter und die Kammer des Schreckens", "J.K. Rowling", "978-0-4390-6486-6");
+        books[2] = new Book("HarryPotter und der Gefangene von Askaban", "J.K. Rowling", "978-8-8691-8612-7");
+        books[3] = new Book("HarryPotter und der Feuerkelch", "J.K. Rowling", "978-0-7569-1297-0");
+        books[4] = new Book("HarryPotter und der Orden des Phönix", "J.K. Rowling", "978-1-8554-9682-8");
+        books[5] = new Book("HarryPotter und der Halbblutprinz", "J.K. Rowling", "978-0-4397-9132-8");
+        books[6] = new Book("HarryPotter und die Heiligtümer des Todes", "J.K. Rowling", "978-0-5450-2937-7");
+        System.out.println(hp1.toString());
     }
 }
